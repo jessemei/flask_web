898217f8ea6675bcbce3e6f688e716efc3d84653
jesse.mei <jesse_mei@163.com>
2016-05-16 15:38:43 +0800

    公司（1）

--- cmdb/index.py
+++ cmdb/index.py
@@ -1,4 +1,23 @@
 #!usr/bin/env python
 #!coding:utf-8
 from flask import Flask,render_template,request
+from flask.ext.script import Manager
+from livereload import Server
 import os
+app = Flask(__name__)
+manage = Manager(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@manage.command
+def dev():
+    live_server = Server(app.wsgi_app)
+    live_server.watch('**/*.*')
+    live_server.serve(open_url=True)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8888,debug=True)
